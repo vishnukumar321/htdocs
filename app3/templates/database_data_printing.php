31cafc9a32ab46bec6332e7f7446b0d7196769ba
vishnukumar321 <vishnu05357874@gmail.com>
2024-09-16 19:28:48 +0530

    app3/templates/database_data_printing.php updated

--- app3/templates/database_data_printing.php
+++ app3/templates/database_data_printing.php
@@ -1,46 +1,102 @@
 <?php
 include "database.php";
 $conn=database::get_conn();
+
+$q="SELECT * FROM `signup`;";
+$r=$conn->query($q);
+$m=$r->num_rows;
+$m=$m-2;
+$ks=false;
+
 if(isset($_POST['pre'])){
     $value=$_POST['pre'];
-    if($value>0){
+    if($value>1){
         $value=$value-2;
-        if($value>0){
-            $value=$value-2;
-
-        }else{
-           $value=0; 
-        }
-    }
-    else{
+    }else{
         $value=0;
+        $ks="there are no data";
     }
 }else if(isset($_POST['next'])){
     $value=$_POST['next'];
-    $value=$value+2;
+    if($value<$m){
+        $value=$value+2;
+        
+
+    }else{
+        $value=$_POST['next'];
+        $ks="there are no data";
+    }
+
+    
 
 }else{
     $value=0;
 }
-$njan=false;
-if(isset($_POST['njan'])){
-    $input=$_POST['njan'];
-    if($input>0){
-        $value=$input-1;
 
+if(isset($_POST['button'])){
+    $input=$_POST['njan'];
+    $m=$m+2;
+    if($input>0 && $input<=$m){
+        if($input==$m){
+            $input=6;
+        }
+        $input=$input-1;
+        $value=$input;
+    }else{
+        $ks=$input." is not founded....OK🤨<br>try again..😏";
+        $value=$_POST['button'];
     }
+
 }
 $sql="SELECT * FROM `signup` limit $value, 2;";
 $result=$conn->query($sql);
 if($result->num_rows==true){
     ?>
-    <center>
+    <style>
+        body{
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .nalla{
+            height: auto;
+            padding: 10px;
+            background-color: black;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: 1px solid black;
+            border-radius: 30px;
+
+        }
+        .njan,td,tr,th,button,input{
+            border: 1px solid black;
+            margin: 2px;
+            padding: 7px;
+            border-radius: 30px;
+            
+        }
+        .njan{
+            background-color: white;
+        }
+        th,td{
+            background-color: black;
+            color: white;
+        }
+        button:hover{
+            background-color: #00dcff;
+            color: black;
+        }
+    </style>
+    <center class="nalla">
     <form method="post" action="database_data_printing.php">
-    <table  border="1">
+        
+    <table class="njan">
     <tr>
         <button name="pre" value="<?php echo $value; ?>">pre</button>
-        <input type="text" name="njan" placeholder="please enter id">
-        <button>GO</button>
+        <input type="number" name="njan" placeholder="please enter id">
+        <button name="button" value="<?php echo $value?>">GO</button>
         <button name="next" value="<?php echo $value; ?>">next</button>
     </tr>
         <tr>
@@ -65,6 +121,9 @@ if($result->num_rows==true){
     
     
     </table>
+    <?php if($ks){
+        echo $ks;
+    } ?>
     </form>
     </center>
     <?php
